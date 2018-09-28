82fc27b98c767a0590041f29abcdb228a91550af
Stephanie Yang <syang93@jhmi.edu>
2018-09-28 15:10:26 -0400

    fixed examine_transcript
    can now feed in covariates to correct for

--- R/examine_transcript.R
+++ R/examine_transcript.R
@@ -10,13 +10,14 @@
 #' @examples
 #' examine_transcript(blood_full, 'ENSG00000141527.16')
 
-examine_transcript <- function(lm_data, transcript, indiv = 304){
+examine_transcript <- function(lm_data, transcript, indiv = 304, correct_for = 'as.factor(lm_data$SEX) + as.numeric(lm_data$AGE) + as.numeric(lm_data$RACE) +
+                  lm_data$PC1 + lm_data$PC2 + lm_data$PC3 + lm_data$PC4 + lm_data$PC5 + lm_data$PC6 + lm_data$PC7 + lm_data$PC8 + lm_data$PC9 + lm_data$PC10'){
   require(ggplot2)
   resids<-as.data.frame(matrix(NA, nrow=indiv, ncol=2))
   colnames(resids) <- c('transcript', 'CN')
   index <- which(colnames(lm_data) == transcript)
-  lm_test <- lm(lm_data[,index] ~ as.factor(lm_data$SEX) + as.numeric(lm_data$AGE) + as.numeric(lm_data$RACE) +
-                  lm_data$PC1 + lm_data$PC2 + lm_data$PC3 + lm_data$PC4 + lm_data$PC5 + lm_data$PC6 + lm_data$PC7 + lm_data$PC8 + lm_data$PC9 + lm_data$PC10, na.action=na.exclude)
+  form <- as.formula(paste0('lm_data[,index] ~ ', correct_for))
+  lm_test <- lm(form, na.action=na.exclude)
   resids$transcript<-scale(resid(lm_test))
   resids$CN<-lm_data$mtDNA_adjust_AGE
   resids <- na.omit(resids)
@@ -42,4 +43,4 @@ examine_transcript <- function(lm_data, transcript, indiv = 304){
 # examine_transcript(lm_full, 'ENSG00000140521.12', 308) + ggtitle('POLG')
 # examine_transcript(lm_full, "ENSG00000108064.10", 308) + ggtitle('TFAM')
 # examine_transcript(lm_full, "ENSG00000186814.12", 308) + ggtitle('ZSCAN30')
-# examine_transcript(lm_full, "ENSG00000178057.14", 291) + ggtitle('NDUFAF3')
+# examine_transcript(lm_full, "ENSG00000178057.14", 291) + ggtitle('NDUFAF3')g
