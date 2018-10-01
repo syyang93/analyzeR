b0b6113fa4a3a0a732137cc005afddc49936d74f
Stephanie Yang <syang93@jhmi.edu>
2018-10-01 13:00:09 -0400

    updated lm test
    now can filter out outliers in gene expression

--- R/lm_test.R
+++ R/lm_test.R
@@ -5,7 +5,7 @@
 #' @param count Our independent variable (numeric vector)
 #' @param covariates Covariates associated with each individual. Columns = covariates, rows = observations. (data.frame)
 #' @param to_correct A string containing additional covariates to correct for.  ex: '+as.factor(covariates$DTHHRDY)+as.factor(covariates$RACE)' 
-#' 
+#' @param outlier_sd Outliers that are greater than outlier_sd standard devs from the mean will be filtered out
 #' @export
 #' 
 #' @return Coefficients from the regression
@@ -15,13 +15,14 @@
 
 
 
-lm_test <- function(lm_data, count, covariates = '', correct_for = ''){
+lm_test <- function(lm_data, count, covariates = '', correct_for = '', outlier_sd = 3){
   count <- as.numeric(count)
 
   # rnaseq outliers > 3SD from the mean --> turn into NAs
-  m <- mean(count)
-  s <- sd(count)
-  
+  m <- mean(lm_data)
+  s <- sd(lm_data)
+  outliers <- which(lm_data > m + outlier_sd*s | lm_data < m - outlier_sd*s)
+  if(length(outliers) != 0){lm_data[outliers] <- NA}
   formula <- as.formula(paste0('lm_data~count', correct_for))
   lm_MT <- lm(formula, na.action=na.exclude)
   care <- coef(summary(lm_MT))["count",]
